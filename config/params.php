24e39bcf5962d310b92041b80d850d9b5baaaacf
Arafeh <arafeh198@gmail.com>
2019-03-23 05:23:59 +0100

    fixes

--- config/params.php
+++ config/params.php
@@ -2,4 +2,10 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    'timezone' => 'Asia/Beirut',
+    'dateFormat' => 'yyyy-mm-dd',
+    'timeFormat' => 'h:i:s',
+    'dateTimeFormat' => 'y-m-d h:i:s',
+    'titlesSelector' => ['Mr' => 'Mr', 'Dr' => 'Dr', 'Mrs' => 'Mrs', 'Ms' => 'Ms'],
+    'booleanSelector' => ['1' => 'YES', '0' => 'NO'],
 ];
